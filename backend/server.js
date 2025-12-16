ab5cc4e85a4e7aa41396b0a1c68760f81bb28701
AshikaWijerathna <wijerathnaashika@gamil.com>
2025-12-16 12:46:32 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,8 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    //origin: "http://localhost:5173",
+    origin:"*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
 }))
